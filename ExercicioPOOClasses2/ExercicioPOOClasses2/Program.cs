707acc84cddbe9c727ff6e376c069b8073b9c57f
Fabio da Fonseca <fmadafonseca@gmail.com>
2022-05-14 08:30:06 -0300

    Adicionados exercicios de POO 1,2 e 3

---
+++ ExercicioPOOClasses2/ExercicioPOOClasses2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace ExercicioPOOClasses2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Funcionario func = new Funcionario();
+        double porcentagem;
+
+        Console.Write("Nome: ");
+        func.Nome = Console.ReadLine();
+        Console.Write("Salário bruto: ");
+        func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Imposto: ");
+        func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.WriteLine("Funcionário: "+ func);
+
+        Console.WriteLine();
+        Console.Write("Digite a porcentagem para aumentar o salário: ");
+        porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        func.AumentarSalario(porcentagem);
+
+        Console.WriteLine();
+        Console.Write("Dados Atualizados: " + func);
+    }
+}
---
+++ exercicioPOOClasses/exercicioPOOClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+namespace exercicioPOOClasses;
+
+class Program {
+    static void Main(string[] args)
+    {
+        Retangulo r = new Retangulo();
+
+        Console.WriteLine("Entre a largura e altura do retângulo:");
+        r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Area = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Perímetro = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Diagonal = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
