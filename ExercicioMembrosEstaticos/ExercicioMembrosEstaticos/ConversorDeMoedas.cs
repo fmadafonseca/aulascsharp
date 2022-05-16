50f18b8d7195871194dd8c465fead9e109403cb4
Fabio da Fonseca <fmadafonseca@gmail.com>
2022-05-16 08:28:34 -0300

    Aulas e Exercicios de Membros Estaticos

---
+++ ExercicioMembrosEstaticos/ExercicioMembrosEstaticos/ConversorDeMoedas.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ExercicioMembrosEstaticos;
+
+internal class ConversorDeMoedas
+{
+    public static double Iof = 6.0;
+    public static double Conversao(double valorDolar, double qtdeDolar)
+    {
+        double total = ((valorDolar * qtdeDolar) + (valorDolar * qtdeDolar)) * Iof / 100;
+        return total + valorDolar * qtdeDolar;
+    }
+}
---
+++ ExercicioMembrosEstaticos/ExercicioMembrosEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+/*Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai
+pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe ConversordeMoeda
+para ser responsável pelos cálculos*/
+namespace ExercicioMembrosEstaticos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe a cotação atual do dólar: ");
+        double valorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Informe quantos dólares você vai comprar: ");
+        double qtdeDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double total = ConversorDeMoedas.Conversao(valorDolar, qtdeDolar);
+
+        Console.WriteLine("Valor a ser pago em reais: " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+}
