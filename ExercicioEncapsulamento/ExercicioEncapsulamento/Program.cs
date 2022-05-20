6d822eddd8d7acd42418581cf6a00240bb404890
Fabio da Fonseca <fmadafonseca@gmail.com>
2022-05-20 09:08:40 -0300

    Adicionado exercicio de Encapsulamento

---
+++ ExercicioEncapsulamento/ExercicioEncapsulamento/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Globalization;
+namespace ExercicioEncapsulamento;
+
+class program
+{
+    static void Main(String[] args)
+    {
+        Conta conta1;
+
+        Console.Write("Entre o número da conta: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.Write("Entre o titular da conta: ");
+        string titular = Console.ReadLine();
+
+        Console.Write("Haverá depósito inicial(s/n)? ");
+        char resposta = char.Parse(Console.ReadLine());
+
+        Conta Conta1 = new Conta(numero, titular, 0);
+
+        if (resposta == 's' || resposta == 'S')
+        {
+            Console.Write("Entre o valor do depósito inicial: " );
+            double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta1 = new Conta(numero, titular, depositoInicial);
+        }
+        else
+        {
+            conta1 = new Conta(numero, titular);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Dados da conta: " + conta1);
+
+        Console.WriteLine();
+        Console.Write("Entre um valor para depósito: ");
+        double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta1.Deposito(deposito);
+        Console.WriteLine("Dados da conta atualizados: " + conta1);
+
+        Console.WriteLine();
+        Console.Write("Entre um valor para saque: ");
+        double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        conta1.Saque(saque);
+        Console.WriteLine("Dados da conta atualizados: " + conta1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
